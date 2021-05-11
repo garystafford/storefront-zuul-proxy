300c169f45b4d1487398c9d2801cbe9eb049fe75
Gary A. Stafford <garystafford@rochester.rr.com>
2021-05-10 22:17:26 -0400

    Updating for 2021.05 release

--- Docker/docker-build-push.sh
+++ Docker/docker-build-push.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ./gradlew clean build
-docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-zuul:latest .
-docker push garystafford/storefront-zuul:latest
+docker build -f Docker/Dockerfile --no-cache -t garystafford/storefront-zuul:3.0.0 .
+docker push garystafford/storefront-zuul:3.0.0
 
-# docker run --name storefront-zuul -d garystafford/storefront-zuul:latest
+# docker run --name storefront-zuul -d garystafford/storefront-zuul:3.0.0
